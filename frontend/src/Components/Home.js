68b304f6ed4d48fad344b7a6f4e839dae4d53aa4
Hjleyva123 <haroldjeffleyva@gmail.com>
2023-01-05 16:41:23 +0800

    first commit

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -39,7 +39,7 @@ function Home() {
             
 
 
-<center><h1>HOMEPAGE</h1>
+<center><h1> In myHOMEPAGE</h1>
 <label>Task</label><br></br>
 <input type="text" id="Task" onChange={(e)=> {
     setTask(e.target.value)
